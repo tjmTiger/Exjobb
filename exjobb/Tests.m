d76f65e158fdb09e3335125ba562df3386b3cfa3
tjm_tiger <tomekmazurek1605@gmail.com>
2025-06-25 17:45:06 +0200

    why is it so slow?

---
+++ exjobb/Tests.m
@@ -0,0 +1,26 @@
+classdef Tests < handle
+%TESTS Summary of this class goes here
+%   Detailed explanation goes here
+
+properties
+    % number_of_tests {mustBeNumeric} = 200 => fix so that all tasts have
+    % same sample size !!!!
+    tests {mustBeCell} = {};
+end
+
+methods
+    function self = Tests()
+        %TESTS Construct an instance of this class
+        %   Detailed explanation goes here
+    end
+    
+    function run(self,varargin)
+        %METHOD1 Summary of this method goes here
+        %   Detailed explanation goes here
+        t = Test(varargin{:});
+        t.run()
+        self.tests{end+1} = t;
+    end
+end
+end
+
--- exjobb/main_new.m
+++ exjobb/main_new.m
@@ -1,7 +1,7 @@
 clear; clc;
 
 
-sample_size = 10; % sample size of graph
+sample_size = 1; % sample size of graph
 %-----------------------------------------------%
 %                                               %
 %     Distrubance and target node fractions     %
@@ -9,11 +9,13 @@ sample_size = 10; % sample size of graph
 %-----------------------------------------------%
 
 % fraction and size
+t = Tests();
 for graph_generating_algorithm = {@erdos_renyi, @watts_strogatz, @sfg}
     for n = 100:50:200
         for fraction = 0.05:0.05:0.3
-            test = Test(@WattsStrogatz, [n, 2, 0.2], "number_of_tests", sample_size);
-            test.run()
+            % test = Test(@erdos_renyi, [n, 2, 0.2], "number_of_tests", sample_size);
+            t.run(@erdos_renyi, [n, 2, 0.2], "number_of_tests", sample_size)
+            disp("done")
         end
     end
 end
