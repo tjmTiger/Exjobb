67a220229841cb57967a9064c3f9bc016bc2f065
tjmTiger <136208644+tjmTiger@users.noreply.github.com>
2025-06-25 18:17:29 +0200

    T w T

--- exjobb/main_new.m
+++ exjobb/main_new.m
@@ -14,10 +14,7 @@ for graph_generating_algorithm = {@erdos_renyi, @watts_strogatz, @sfg}
     for n = 100:50:200
         for fraction = 0.05:0.05:0.3
             disp(graph_generating_algorithm)
-            % test = Test(@erdos_renyi, [n, 2, 0.2], "number_of_tests", sample_size);
-            t_start = tic;
             t.run(@erdos_renyi, [n, 2, 0.2], "number_of_tests", sample_size)
-            disp("total time: " + toc(t_start))
         end
     end
 end
